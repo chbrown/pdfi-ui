1cf8459f7a43dff31c9da111a44bc060ddc40c86
Christopher Brown <io@henrian.com>
2015-05-10 17:55:53 -0500

    Refactor virtual-dom out into my DefinitelyTyped fork

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,39 +1,8 @@
 /// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
 /// <reference path="DefinitelyTyped/angularjs/angular-resource.d.ts" />
 /// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/virtual-dom/virtual-dom.d.ts" />
 
-interface VirtualNode {
-  tagName: string;
-  properties: any;
-  children: any[];
-  key?: string;
-  namespace?: string;
-  count: number;
-  hasWidgets: boolean;
-  hasThunks: boolean;
-  hooks: any[];
-  descendantHooks: any[];
-}
-
-declare module "virtual-dom/h" {
-  function h(tagName: string, ...args: any[]): VirtualNode;
-  export = h;
-}
-declare module "virtual-dom/create-element" {
-  function create(vnode: VirtualNode, opts?: any): Element;
-  export = create;
-}
-declare module "virtual-dom/diff" {
-  function diff(a: any, b: any): any[];
-  export = diff;
-}
-declare module "virtual-dom/patch" {
-  /**
-  patch() usually just returns rootNode after doing stuff to it, so we want
-  to preserve that type (though it will usually be just Element).
-  */
-  function patch<T extends Element>(rootNode: T, patches: any[], opts?: any): T;
-  export = patch;
-}
-// var VNode = require('virtual-dom/vnode/vnode');
-// var VText = require('virtual-dom/vnode/vtext');
+// self-declaring packages:
+/// <reference path="../node_modules/academia/academia.d.ts" />
