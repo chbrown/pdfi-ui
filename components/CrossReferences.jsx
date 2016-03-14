d69c34eab6edc5cef4cc777d7f4952deeba5addf
Christopher Brown <io@henrian.com>
2016-03-13 20:33:17 -0500

    Split CrossReferences UI into naive component and @connect'ed view

---
+++ components/CrossReferences.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {CrossReferencePropTypes} from '../propTypes';
+import ObjectView from './ObjectView';
+
+const CrossReferencesTable = ({cross_references}) => (
+  <table className="fill padded striped lined">
+    <thead>
+      <tr>
+        <th>View</th>
+        <th>Object</th>
+        <th>Generation</th>
+        <th>In use?</th>
+        <th className="right">Offset</th>
+      </tr>
+    </thead>
+    <tbody>
+      {cross_references.map(({object_number, generation_number, offset, in_use}) =>
+        <tr key={`${object_number}:${generation_number}`}>
+          <td><ObjectView object={{object_number, generation_number}} /></td>
+          <td>{object_number}</td>
+          <td>{generation_number}</td>
+          <td><ObjectView object={in_use} /></td>
+          <td className="right">{offset}</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+CrossReferencesTable.propTypes = {
+  cross_references: React.PropTypes.arrayOf(React.PropTypes.shape(CrossReferencePropTypes)).isRequired,
+};
+export default CrossReferencesTable;
--- views/CrossReferences.jsx
+++ views/CrossReferences.jsx
@@ -1,49 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import {CrossReferencePropTypes} from '../propTypes';
-import ObjectView from '../components/ObjectView';
-
-
-const CrossReferenceRow = ({object_number, generation_number, offset, in_use}) => (
-  <tr>
-    <td>
-      <ObjectView object={{object_number, generation_number}} />
-    </td>
-    <td>{object_number}</td>
-    <td>{generation_number}</td>
-    <td>{in_use ? 'Yes' : 'No'}</td>
-    <td className="right">{offset}</td>
-  </tr>
-);
-// cross_references: React.PropTypes.arrayOf(React.PropTypes.shape(CrossReferencePropTypes)).isRequired,
-CrossReferenceRow.propTypes = CrossReferencePropTypes;
+import CrossReferencesTable from '../components/CrossReferences';
 
 @connect(state => ({pdf: state.pdf}))
-export default class CrossReferencesTable extends React.Component {
+export default class PDFCrossReferencesTable {
   render() {
     const {pdf} = this.props;
     return (
       <section className="hpad">
         <h2>Cross References</h2>
-        <table className="fill padded striped lined">
-          <thead>
-            <tr>
-              <th>View</th>
-              <th>Object</th>
-              <th>Generation</th>
-              <th>In use?</th>
-              <th>Offset</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pdf.cross_references.map(cross_reference => (
-              <CrossReferenceRow key={`${cross_reference.object_number}:${cross_reference.generation_number}`} {...cross_reference} />
-            ))}
-          </tbody>
-        </table>
+        <CrossReferencesTable cross_references={pdf.cross_references} />
       </section>
     );
   }
-  // cross_references: React.PropTypes.arrayOf(React.PropTypes.shape(CrossReferencePropTypes)).isRequired,
 }
