3e555b9d689576d511207c29155ba4614ec9909d
Christopher Brown <io@henrian.com>
2018-07-08 18:52:46 -0500

    Upgrade typescript + webpack build process; update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
-
-var production = process.env.NODE_ENV == 'production';
+const {resolve} = require('path');
+const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: [
-    './app',
-  ].concat(production ? [] : [
-    'webpack-hot-middleware/client',
-  ]),
+  mode: env,
+  entry: {
+    app: './app',
+  },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/build/',
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(true),
-  ].concat(production ? [
-    // production-only
-    new webpack.optimize.UglifyJsPlugin(),
-  ] : [
-    // development-only
-    new webpack.NoErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ]),
-  resolve: {
-    extensions: [
-      '',
-      '.js',
-      '.jsx',
-      '.json',
-    ],
-  },
-  resolveLoader: {
-    // otherwise, webpack will look for json-loader relative to the modules
-    // themselves, which will break on `npm link`'ed modules
-    root: path.join(__dirname, 'node_modules'),
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        loaders: ['babel-loader'],
+        test: /\.js$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
       },
       {
         test: /\.less$/,
-        loaders: ['style-loader', 'css-loader', 'less-loader'],
-      },
-      {
-        test: /\.json$/,
-        loaders: ['json-loader'],
+        exclude: /node_modules/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'less-loader',
+        }],
       },
     ],
   },
