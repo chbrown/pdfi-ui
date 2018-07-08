ef547530c868b0b7682e31f42a9e7d977d02d136
Christopher Brown <io@henrian.com>
2018-07-08 18:49:10 -0500

    Add util.{bind,assertSuccess,parseContent} and clean up 'graphics' module

--- graphics.ts
+++ graphics.ts
@@ -1,23 +1,16 @@
+import {CSSProperties} from 'react';
 import {Rectangle} from 'pdfi/graphics/geometry';
 
 export function px(length: number, fractionDigits = 3) {
   return length.toFixed(fractionDigits) + 'px';
 }
 
-export function makeBoundsStyle(rectangle: Rectangle): {[index: string]: string} {
-  if (rectangle === undefined) return undefined;
+export function makeBoundsStyle({minX, minY, maxX, maxY}: Rectangle): CSSProperties {
   return {
     position: 'absolute',
-    left: px(rectangle.minX, 3),
-    top: px(rectangle.minY, 3),
-    width: px(rectangle.maxX - rectangle.minX, 3),
-    height: px(rectangle.maxY - rectangle.minY, 3),
+    left: px(minX, 3),
+    top: px(minY, 3),
+    width: px(maxX - minX, 3),
+    height: px(maxY - minY, 3),
   };
-}
-
-export function makeBoundsString(rectangle: Rectangle): string {
-  if (rectangle === undefined) return undefined;
-  const dX = rectangle.maxX - rectangle.minX;
-  const dY = rectangle.maxY - rectangle.minY;
-  return `(${rectangle.minX.toFixed(3)},${rectangle.minY.toFixed(3)}) (${dX.toFixed(3)}x${dY.toFixed(3)})`;
 }
---
+++ util.ts
@@ -0,0 +1,43 @@
+export function bind<T extends Function>(target: Object,
+                                         propertyKey: string | symbol,
+                                         descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
+  return {
+    configurable: true,
+    get(this: T): T {
+      const value = descriptor.value.bind(this);
+      Object.defineProperty(this, propertyKey, {
+        value,
+        configurable: true,
+        writable: true,
+      });
+      return value;
+    },
+  };
+}
+
+/**
+Check that a fetch() Response has a successful status code and turn it into a
+rejected Promise if not.
+*/
+export function assertSuccess<R extends Response>(response: R): Promise<R> {
+  if (response.status < 200 || response.status > 299) {
+    // let error = new Error(`HTTP ${response.status}`);
+    // error['response'] = response;
+    return Promise.reject<R>(response);
+  }
+  return Promise.resolve(response);
+}
+
+/**
+We can't write the .body property of a Response because it's a read-only getter,
+so we use .content instead.
+*/
+export function parseContent<R extends Response>(response: R): Promise<R & {content: any}> {
+  const contentType = response.headers.get('content-type');
+  if (contentType == 'application/json') {
+    return response.json().then(content => Object.assign(response, {content}));
+  }
+  else {
+    return response.text().then(content => Object.assign(response, {content}));
+  }
+}
